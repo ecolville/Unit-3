ad2e11436dc1cd4900b2595989d09653c7a8d50b
ecolville <88344955+ecolville@users.noreply.github.com>
2023-04-02 20:33:33 -0500

    Activity 10

--- js/main_sandbox.js
+++ js/main_sandbox.js
@@ -11,16 +11,16 @@
     //set up choropleth map
     function setMap() {
         //map frame dimensions
-        var width = window.innerWidth * 0.5,
-            height = 510;
+        var width = 950,
+            height = 610;
 
         //create new svg container for the map
         var map = d3.select("body")
             .append("svg")
             .attr("class", "map")
             .attr("width", width)
-            .attr("height", height);
-    
+            .attr("height", height)
+                
         //create US-centric composite projection of three Albers equal area conic projections...does the hard work of including AK and HI for me!
         var projection = d3.geoAlbersUsa();
             
@@ -120,98 +120,14 @@
 
 	    return colorScale;
 	};
- 
-    //function to create coordinated bar chart
-    function setChart(csvData, colorScale){
-        //chart frame dimensions
-        var chartWidth = window.innerWidth * 0.425,
-            chartHeight = 523,
-            leftPadding = 25,
-            rightPadding = 2,
-            topBottomPadding = 5,
-            chartInnerWidth = chartWidth - leftPadding - rightPadding,
-            chartInnerHeight = chartHeight - topBottomPadding * 2,
-            translate = "translate(" + leftPadding + "," + topBottomPadding + ")";
-
-        //create a second svg element to hold the bar chart
-        var chart = d3.select("body")
-            .append("svg")
-            .attr("width", chartWidth)
-            .attr("height", chartHeight)
-            .attr("class", "chart");
-
-        //create a rectangle for chart background fill
-        var chartBackground = chart.append("rect")
-            .attr("class", "chartBackground")
-            .attr("width", chartInnerWidth)
-            .attr("height", chartInnerHeight)
-            .attr("transform", translate);
-
-        //create a scale to size bars proportionally to frame and for axis
-        var yScale = d3.scaleLinear()
-            .range([0, chartHeight])
-            .domain([20, 0]);
-
-        //set bars for each state
-        var bars = chart.selectAll(".bars")
-            .data(csvData)
-            .enter()
-            .append("rect")
-            .sort(function(a, b){
-                return a[expressed] - b[expressed]
-            })
-            .attr("class", function(d){
-                return "bars " + d.diss_me;
-            })
-            .attr("width", chartInnerWidth / csvData.length - 1)
-            .attr("x", function(d, i){
-                return i * (chartInnerWidth / csvData.length) + leftPadding;
-            })
-            .attr("height", function(d, i){
-                return 513- yScale(parseFloat(d[expressed]));
-            })
-            .attr("y", function(d, i){
-                return yScale(parseFloat(d[expressed])) + topBottomPadding;
-            })
-            .style("fill", function(d){
-                return colorScale(d[expressed]);
-            });
-        
-        //annotate bars with attribute value text
-        var numbers = chart.selectAll(".numbers")
-            .data(csvData)
-            .enter()
-            .append("text")
-            .sort(function(a, b){
-                return a[expressed] - b[expressed]
-            })
-            .attr("class", function(d){
-                return "numbers " + d.diss_me;
-            })
-            .attr("text-anchor", "middle")
-            .attr("x", function(d, i){
-                var fraction = chartInnerWidth / csvData.length;
-                    return i * fraction + (fraction - 1) / 2;
-            })
-            .attr("y", function(d){
-                return chartInnerHeight - yScale(parseFloat(d[expressed])) + 15;
-            })
-            .text(function(d){
-                return d[expressed];
-            });
-                
-        //create a text element for the chart title
-        var chartTitle = chart.append("text")
-            .attr("x", 40)
-            .attr("y", 40)
-            .attr("class", "chartTitle")
-            .text("2020: " + expressed.split("_")[0] + " " + expressed.split("_")[1] + " " + expressed.split("_")[2] + " " + expressed.split("_")[3] + " " + expressed.split("_")[4]);
-
-        //create frame for chart border
-        var chartFrame = chart.append("rect")
-            .attr("class", "chartFrame")
-            .attr("width", chartInnerWidth)
-            .attr("height", chartInnerHeight)
-            .attr("transform", translate);
-    };
+    
+    //add tooltip
+    var tooltip = d3.select("body").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0)
+    
+    //add clickable background
+    svg.append("rect")
+        .attr("class", "background")
+        .attr("width", innerWidth)
 })(); 
