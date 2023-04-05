19a41fc6eb2e75200cd03d68dae2c5b1c438a53c
ecolville <88344955+ecolville@users.noreply.github.com>
2023-04-04 21:58:38 -0500

    Activity 11

--- js/main.js
+++ js/main.js
@@ -215,4 +215,27 @@
             .attr("height", chartInnerHeight)
             .attr("transform", translate);
     };
+    
+    //function to create a dropdown menu for attribute selection
+    function createDropdown(){
+        //add select element
+        var dropdown = d3.select("body")
+            .append("select")
+            .attr("class", "dropdown");
+
+        //add initial option
+        var titleOption = dropdown.append("option")
+            .attr("class", "titleOption")
+            .attr("disabled", "true")
+            .text("Select Attribute");
+
+        //add attribute name options
+        var attrOptions = dropdown.selectAll("attrOptions")
+            .data(attrArray)
+            .enter()
+            .append("option")
+            .attr("value", function(d){ return d })
+            .text(function(d){ return d });
+    };   
+    
 })(); 
